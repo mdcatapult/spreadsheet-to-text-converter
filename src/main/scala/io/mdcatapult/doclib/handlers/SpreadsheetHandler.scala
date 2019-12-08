a1c24b7255cccb1286d1bd373c35bc3b19b54b49
Matt Cockayne <matthew.cockayne@md.catapult.org.uk>
2019-12-08 10:55:27 +0000

    adds explicit exception for imietype errors

--- src/main/scala/io/mdcatapult/doclib/handlers/SpreadsheetHandler.scala
+++ src/main/scala/io/mdcatapult/doclib/handlers/SpreadsheetHandler.scala
@@ -10,6 +10,7 @@ import cats.data.OptionT
 import cats.implicits._
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
+import io.mdcatapult.doclib.exception.DoclibDocException
 import io.mdcatapult.doclib.messages.{DoclibMsg, PrefetchMsg, SupervisorMsg}
 import io.mdcatapult.doclib.models.metadata.{MetaString, MetaValueUntyped}
 import io.mdcatapult.doclib.models.{Derivative, DoclibDoc, Origin}
@@ -23,7 +24,7 @@ import org.mongodb.scala.model.Filters.equal
 import org.mongodb.scala.model.Updates.{combine, set}
 import org.mongodb.scala.result.UpdateResult
 
-import scala.concurrent.duration._
+import scala.concurrent.duration._git pull origin develop
 import scala.concurrent.{Await, ExecutionContextExecutor, Future}
 import scala.language.postfixOps
 import scala.util.{Failure, Success, Try}
@@ -34,6 +35,13 @@ class SpreadsheetHandler(downstream: Sendable[PrefetchMsg], supervisor: Sendable
                          ex: ExecutionContextExecutor,
                          config: Config,
                          collection: MongoCollection[DoclibDoc]) extends LazyLogging {
+
+  final case class MimetypeNotAllowed(doc: DoclibDoc,
+                                cause: Throwable = None.orNull)
+    extends DoclibDocException(
+      doc,
+      f"Document: ${doc._id.toHexString} - Mimetype '${doc.mimetype}' not allowed'",
+      cause)
 
   lazy val flags = new DoclibFlags(config.getString("doclib.flag"))
 
@@ -67,6 +75,7 @@ class SpreadsheetHandler(downstream: Sendable[PrefetchMsg], supervisor: Sendable
         case None ⇒ // do nothing?
       }
       // Wait 10 seconds then fail
+      case Failure(e: DoclibDocException) ⇒ flags.error(e.getDoc, noCheck = true)
       case Failure(_) ⇒ Try(Await.result(collection.find(equal("_id", new ObjectId(msg.id))).first.toFutureOption(), 10 seconds)) match {
         case Success(value: Option[DoclibDoc]) ⇒ value match {
           case Some(aDoc) ⇒ flags.error(aDoc, noCheck = true)
@@ -87,7 +96,7 @@ class SpreadsheetHandler(downstream: Sendable[PrefetchMsg], supervisor: Sendable
       """application/vnd\.oasis\.opendocument\.spreadsheet""".r
     ).count(_.findFirstIn(doc.mimetype).isDefined) > 0) {
       Some(true)
-    } else throw new Exception("Document mimetype is not recognised")
+    } else throw new MimetypeNotAllowed(doc)
   }
 
   /**
