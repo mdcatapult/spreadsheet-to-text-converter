4dc43498496910e14256a3c5c92cc3b61f06aeb6
Ian Dunlop <ian.dunlop@md.catapult.org.uk>
2023-07-17 14:28:15 +0100

    Revert to previously correct spreadsheet tab assertions

--- src/it/scala/io/mdcatapult/doclib/consumers/ConsumerSpreadsheetConverterIntegrationTest.scala
+++ src/it/scala/io/mdcatapult/doclib/consumers/ConsumerSpreadsheetConverterIntegrationTest.scala
@@ -43,8 +43,7 @@ class ConsumerSpreadsheetConverterIntegrationTest extends TestKit(ActorSystem("S
   with ScalaFutures
   with BeforeAndAfterAll {
 
-//  val sheets: Map[String, Int] = Map[String, Int]( "/test.csv" -> 1, "/test.xls" -> 2, "/test.xlsx" -> 2, "test.ods" -> 2, "/test.xlsx" -> 3)
-  val sheets: Map[String, Int] = Map[String, Int]( "/test.csv" -> 1, "/test.xls" -> 2, "test.ods" -> 2, "/test.xlsx" -> 3)
+  val sheets: Map[String, Int] = Map[String, Int]( "/test.csv" -> 1, "/test.xls" -> 2, "/test.xlsx" -> 2, "test.ods" -> 2)
 
   implicit val config: Config = ConfigFactory.load()
 
